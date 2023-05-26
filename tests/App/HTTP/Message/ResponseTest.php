7266aea5ba576a9ff27f9f14a69c987073639a37
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2023-05-26 14:34:44 +0300

    upd tests

--- tests/App/HTTP/Message/ResponseTest.php
+++ tests/App/HTTP/Message/ResponseTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests\App\HTTP\Message;
 
 use Framework\HTTP\Message\Response;
+use Framework\HTTP\Message\Stream;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -14,7 +15,7 @@ final class ResponseTest extends TestCase
 {
     public function testGetStatusCode(): void
     {
-        $response = new Response(201);
+        $response = new Response(201, null);
         self::assertEquals(201, $response->getStatusCode());
     }
 
@@ -24,13 +25,14 @@ final class ResponseTest extends TestCase
             'Accept-Language' => 'ru',
             'Request-Method' => 'POST',
         ];
-        $response = new Response(201, headers: $headers);
+        $response = new Response(201, null, $headers);
         self::assertEquals($headers, $response->getHeaders());
     }
 
     public function testGetBody(): void
     {
-        $body = 'Good morning!';
+        $body = new Stream(fopen('php://memory', 'rb+'));
+        $body->write('Good morning!');
         $response = new Response(201, body: $body);
         self::assertEquals($body, $response->getBody());
     }
